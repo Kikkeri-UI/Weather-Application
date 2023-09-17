a37ea195d24659336e0c4cd83918ac151fb5bb3b
Kikkeri-UI <sagarbharadwaj98@gmail.com>
2023-09-17 18:55:47 +0100

    Minor Changes

--- weatherApp/src/app/app.component.ts
+++ weatherApp/src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { WeatherService } from './services/weather.service';
 import { WeatherData } from './models/weather.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable({ providedIn: 'root' })
 export class AppComponent implements OnInit {
 
-  constructor(private weatherService:WeatherService){}
+
+constructor(private weatherService:WeatherService, private toastr:ToastrService){}
   weatherData?:WeatherData;
   cityName:string='London';
 
@@ -25,12 +28,29 @@ export class AppComponent implements OnInit {
   private getWeatherData(cityName:string){
     this.weatherService.getWeatherData(cityName)
     .subscribe({
+      
       next :(response)=>{
-        this.weatherData=response;
+        if(response){
+          this.weatherData=response;
+        let icon=response.weather[0].icon;
+        let iconUrl="http://openweathermap.org/img/w/" + icon + ".png";
+        console.log(iconUrl);
+        console.log(iconUrl);
+        console.log(icon);
         console.log(response);
+        }
+        else{
+          this.toastr.info('something wrong please try leater');
+        }
+        
 
-      }
-    })
+      },
+      
+    });
+    
+    
+    
+    
 
   }
 }
